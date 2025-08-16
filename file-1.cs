e64ff448c0c2b0dcf4799e9b831a572d1a70dee1
sruja <113293177+srxja@users.noreply.github.com>
2025-08-16 22:53:42 +0530

    Create file-1.cs

---
+++ file-1.cs
@@ -0,0 +1,12 @@
+Console.Writeline(); //appends new line after the parameter
+Console.Write(); //doesnt append new line
+Console.WriteLine('a'); //char literal
+Console.WriteLine("Hello"); //string, USE DOUBLE QUOTES
+Console.WriteLine(2.455f); //float, can use F too ~6-9 digits
+Console.WriteLine(24.555); //double, ~15-17 digits
+Console.WriteLine(32.444m); //decimal, ~28-29 digits
+Console.WriteLine(true); //or false
+
+//note
+// prefer string datatype when working with numerical data that does not involve arithmetic, say postal codes for example
+// use bool for concept of true and false, string if you need actual "true" or "false"
